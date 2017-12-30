486397f91411e1c6dc30c58f3ed1aee882ec8bd1
chrisofarabia <chrisofarabia@yahoo.co.uk>
2017-12-30 16:30:09 +0000

    Added POST route for comments

--- routes/comments.js
+++ routes/comments.js
@@ -6,7 +6,11 @@ const commentRoutes = {
     res.status(200).send(post.comments);
   },
   addComment(req, res) {
-
+    const post = db.posts[req.params.postId]
+    const newComment = req.body             // Obtains the content of the new post from the request body
+    const id = post.comments.length         // Creates a new ID based on the current number of comments
+    posts.comments.push(newComment)         // Pushes the new post to the end of the db.posts array
+    res.status(201).send({ id: id })        // Issues a 201 (Created) status and sends the ID of the new record
   },
   updateComment(req, res) {
 
