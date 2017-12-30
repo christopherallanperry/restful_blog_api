7cff3e869e00daa682b274c8508b8aba7110e321
chrisofarabia <chrisofarabia@yahoo.co.uk>
2017-12-30 14:55:42 +0000

    Completed POST localhost:3000/posts inc. basic functional test

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const posts = require("./posts");
-// const comments = require("./comments");
+const comments = require("./comments");
 
 module.exports = (baseUrl, app) => {
   app.get(baseUrl + "posts", posts.getPosts)
+  app.post(baseUrl + "posts", posts.addPost)
 
 }
--- routes/posts.js
+++ routes/posts.js
@@ -2,13 +2,17 @@ const db = require("./db");
 
 const postRoutes = {
   getPosts(req, res) {
-    res.status(200).send(db.posts)
+    res.status(200).send(db.posts)    // Issues a 200 (OK) status and sends the content of db.posts
   },
   addPost(req, res) {
-
+    const id = db.posts.length        // Creates a new ID based on the current number of posts
+    const newPost = req.body          // Obtains the content of the new post from the request body
+    newPost.comments = []             // Sets the comments for the new post to an empty array
+    db.posts.push(newPost)            // Pushes the new post to the end of the db.posts array
+    res.status(201).send({ id: id })  // Issues a 201 (Created) status and sends the ID of the new record
   },
   updatePost(req, res) {
-
+    
   },
   removePost(req, res) {
 
